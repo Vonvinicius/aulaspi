2b11c7ed9f0c49bc415eb58215682b0e5ed9b8a5
Igor Vinícius da Silva Matias <igor.vinicius@academico.ifrn.edu.br>
2022-10-21 11:48:52 -0300

    Criando a listagem de eventos

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,25 +1,30 @@
 package ifrn.pi.eventos.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import ifrn.pi.eventos.models.Evento;
 import ifrn.pi.eventos.repositories.EventoRepository;
 
 @Controller
+@RequestMapping("/eventos")
 public class EventosController {
 	
 	@Autowired
 	private EventoRepository er;
 	
-	@RequestMapping("/eventos/form")
+	@GetMapping("/form")
 	public String form() {
 		return "eventos/formEvento";
 	}
 	
-	@RequestMapping("/Eventos")
+	@RequestMapping
 	public String nome(String nome){
 		System.out.println("Nome do evento: " + nome);
 		return "verificar";
@@ -29,5 +34,13 @@ public class EventosController {
 		System.out.println(evento);
 		er.save(evento);
 		return "eventos/evento-adicionado";
+	}
+	@GetMapping("/eventos")
+	public ModelAndView listar() {
+			List<Evento> eventos = er.findAll();
+			ModelAndView mv = new ModelAndView("eventos/lista");
+			mv.addObject("eventos", eventos);
+			return mv;
+		
 	}
 }
